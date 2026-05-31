a15c1f5539c2944fe92cf04d83e60f97961f7724
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2026-05-31 07:54:23 +0000

    mediatek: scp: Import Xiaomi changes
    Signed-off-by: AbzRaider <99021451+AbzRaider@users.noreply.github.com>
    
    (cherry picked from commit 0cf846aa89ae17582b3110b99bdf0d21b142c92b)

--- drivers/misc/mediatek/scp/rv/scp_feature_define.h
+++ drivers/misc/mediatek/scp/rv/scp_feature_define.h
@@ -17,7 +17,12 @@
 #define SCP_LOGGER_ENABLE                (0)
 #endif
 #define SCP_DVFS_INIT_ENABLE             (1)
+#define SCP_LOGGER_OVERWRITE		(1)
 #define SCP_VOW_LOW_POWER_MODE           (1)
+
+#ifdef SCP_LOGGER_OVERWRITE
+#define HW_SEM_LOGGER		        (1)
+#endif
 
 /* scp rescovery feature option*/
 #define SCP_RECOVERY_SUPPORT             (1)
--- drivers/misc/mediatek/scp/rv/scp_logger.c
+++ drivers/misc/mediatek/scp/rv/scp_logger.c
@@ -232,8 +232,21 @@ ssize_t scp_A_log_read(char __user *data, size_t len)
 
 	mutex_lock(&scp_A_log_mutex);
 
-	r_pos = SCP_A_buf_info->r_pos;
+
 	w_pos = SCP_A_buf_info->w_pos;
+	
+	#ifdef SCP_LOGGER_OVERWRITE
+	//pr_err("scp_A_log_read: len= %d\n", len);
+
+	if (get_scp_semaphore(HW_SEM_LOGGER) < 0) {
+		pr_err("[SCP]: HW_semaphore Get fail.\n");
+		mutex_unlock(&scp_A_log_mutex);
+		return 0;
+	}
+#endif
+
+	r_pos = SCP_A_buf_info->r_pos;
+
 
 	if (r_pos == w_pos)
 		goto error;
@@ -270,6 +283,13 @@ ssize_t scp_A_log_read(char __user *data, size_t len)
 	SCP_A_buf_info->r_pos = r_pos;
 
 error:
+#ifdef SCP_LOGGER_OVERWRITE
+	if (release_scp_semaphore(HW_SEM_LOGGER) < 0) {
+		pr_err("[SCP]: HW_semaphore Release fail.\n");
+		mutex_unlock(&scp_A_log_mutex);
+		return 0;
+	}
+#endif
 	mutex_unlock(&scp_A_log_mutex);
 
 	return datalen;
@@ -517,8 +537,20 @@ static ssize_t scp_A_mobile_log_UT_show(struct device *kobj,
 
 	mutex_lock(&scp_A_log_mutex);
 
-	r_pos = SCP_A_buf_info->r_pos;
+	
 	w_pos = SCP_A_buf_info->w_pos;
+	
+	#ifdef SCP_LOGGER_OVERWRITE
+	pr_err("scp_A_log_read: len= %d\n", len);
+
+	if (get_scp_semaphore(HW_SEM_LOGGER) < 0) {
+		pr_err("[SCP]: HW_semaphore Get fail.\n");
+		mutex_unlock(&scp_A_log_mutex);
+		return 0;
+	}
+#endif
+
+	r_pos = SCP_A_buf_info->r_pos;
 
 	if (r_pos == w_pos)
 		goto error;
@@ -545,6 +577,13 @@ static ssize_t scp_A_mobile_log_UT_show(struct device *kobj,
 	SCP_A_buf_info->r_pos = r_pos;
 
 error:
+#ifdef SCP_LOGGER_OVERWRITE
+	if (release_scp_semaphore(HW_SEM_LOGGER) < 0) {
+		pr_err("[SCP]: HW_semaphore Release fail.\n");
+		mutex_unlock(&scp_A_log_mutex);
+		return 0;
+	}
+#endif
 	mutex_unlock(&scp_A_log_mutex);
 
 	return len;
