67595825f4319d6e884d03586bfb8a59a7b98e45
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2026-05-31 07:54:36 +0000

    drivers: pwm: Import Xiaomi changes
    Signed-off-by: AbzRaider <99021451+AbzRaider@users.noreply.github.com>
    
    (cherry picked from commit 695194b33bffd1fe281ce4cb45f870ab3ae13dff)

--- drivers/pwm/pwm-mtk-disp.c
+++ drivers/pwm/pwm-mtk-disp.c
@@ -185,6 +185,7 @@ static int mtk_disp_pwm_probe(struct platform_device *pdev)
 	int ret;
 
 	pr_notice("%s start", __func__);
+	pr_notice("%s start\n", __func__);
 	mdp = devm_kzalloc(&pdev->dev, sizeof(*mdp), GFP_KERNEL);
 	if (!mdp)
 		return -ENOMEM;
@@ -243,6 +244,7 @@ static int mtk_disp_pwm_probe(struct platform_device *pdev)
 					 mdp->data->con0_sel,
 					 mdp->data->con0_sel);
 	}
+	pr_notice("%s end\n", __func__);
 
 	pr_notice("%s end", __func__);
 	return 0;
